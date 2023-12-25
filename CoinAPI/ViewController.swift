35faa669e39c3aa7d8176e30d083d98a290f8d0c
SnakCat <snakkdss@gmail.com>
2023-12-25 20:51:25 +0300

    add UI viewController

--- CoinAPI/ViewController.swift
+++ CoinAPI/ViewController.swift
@@ -1,13 +1,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - private propertys
+    private let titleLabel = UILabel()
+    private let tableView = UITableView()
 
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+        setupConstreints()
+        setupUI()
+        configTableView()
     }
-
-
+    
+    // MARK: - helpers methods
+    // table view confug
+    private func configTableView() {
+        tableView.dataSource = self
+    }
+    
+    // constreints
+    private func setupConstreints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 70),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // UI
+    private func setupUI() {
+        titleLabel.text = "Exhange Rates"
+        titleLabel.font = .systemFont(ofSize: 30)
+    }
 }
+// MARK: - extension
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
 
